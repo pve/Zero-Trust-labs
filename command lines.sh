6872302209ef5bfc9a1dc43b3081fc28df2d3b5d
Peter van Eijk <peter@peterhjvaneijk.nl>
2025-01-02 12:15:43 +0100

    update for demo

--- command lines.sh
+++ command lines.sh
@@ -1,14 +1,26 @@
-export KUBECONFIG=k8s-1-31-1-do-5-ams3-1735750671919-kubeconfig.yaml
+export KUBECONFIG=kubeconfig.yaml
+
+# let's see if we have a Kubernetes cluster
+kubectl get nodes
+
+# run a few apps
 kubectl run busybox1 --image=busybox --labels app=busybox1 -- sleep 3600
 kubectl run busybox2 --image=busybox --labels app=busybox2 -- sleep 3600
 
-kubectl get pods -o wide
 
-# will give you the ips of the pods
+
+kubectl get pods -o wide
+# note the ips of the pods
+
 kubectl exec -ti busybox2 -- ping -c3 <ip address>
 kubectl exec -ti busybox2 -- ping -c3  10.108.0.6
-
+# now you see me
 kubectl apply -f networkpolicy.yaml
+
+# now you don't 
+
+kubectl get networkpolicy
+kubectl describe networkpolicy web-deny-all
 
 # Cleanup: 
 kubectl delete pod busybox1 busybox2
